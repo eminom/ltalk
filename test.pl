2bdc01db8c235af179cdcc37287ef29ab4012b98
jokerbai <hai.bai.cn@gmail.com>
2014-09-29 19:31:13 -0700

    add test

---
+++ test.pl
@@ -0,0 +1,41 @@
+#! /usr/bin/perl -w
+
+use 5.010;
+use strict;
+use warnings;
+use Cwd qw/getcwd/;
+
+sub getFileList{
+	my $cur = shift // die "no inputs";
+	my @rv = ();
+	opendir my $cd, $cur or die "cannot open $cur";
+	while ( my $f = readdir $cd ){
+		my $ff = $cur . '/' . $f;
+		next if not -f $ff;
+		#print $ff,"\n";
+		push @rv, $ff;
+	}
+	closedir $cd;
+	@rv;
+}
+
+sub main{
+	my $cur = getcwd() . '/input';
+	my @arr =	getFileList $cur;
+	my $passed = 0;
+	for(@arr){
+		#print $_,"\n";
+		my $cmd = "./parser < $_";
+		`$cmd`;
+		die "test failed: $_" if $?;
+		$passed ++;
+	}
+
+	say "All $passed passed";
+
+}
+
+
+#Entry
+main;
+
