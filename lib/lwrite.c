22a3c7c35f466b2b244c84ebfc1268eed956bc12
jokerbai <hai.bai.cn@gmail.com>
2014-09-29 23:41:31 -0700

    yes

--- lib/lwrite.c
+++ lib/lwrite.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lwrite.h"
-#include "json/cJSON.h"
+#include "../json/cJSON.h"
 
 void paramEntry_writeRecursive(ParamEntry *param){
 	if(!param){return;}
